33288f0c618c3edec74447b95f9c50d6afd947b0
Positivohd1 <caiorafaellw@gmail.com>
2025-05-20 15:22:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,25 +7,4 @@ let currentIndex = 0;
 
 function showDepo(index) {
   depoimentos.forEach((depo, i) => {
-    depo.classList.toggle('active', i === index);
-  });
-}
-
-function nextDepo() {
-  currentIndex = (currentIndex + 1) % depoimentos.length;
-  showDepo(currentIndex);
-}
-
-function prevDepo() {
-  currentIndex = (currentIndex - 1 + depoimentos.length) % depoimentos.length;
-  showDepo(currentIndex);
-}
-
-nextBtn.addEventListener('click', nextDepo);
-prevBtn.addEventListener('click', prevDepo);
-
-// Auto play a cada 6 segundos
-setInterval(nextDepo, 6000);
-
-// Mostrar o primeiro depoimento ao carregar
-showDepo(currentIndex);
+   
